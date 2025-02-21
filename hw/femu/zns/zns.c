4eb7f1e042fc54a574649bcb2f710c1e18120266
Ming-Feng Wei <mfwei.personal@gmail.com>
2025-02-21 07:07:55 +0000

    Add small-zone configuration from default ConfZNS

--- hw/femu/zns/zns.c
+++ hw/femu/zns/zns.c
@@ -4,7 +4,7 @@
 
 #define MIN_DISCARD_GRANULARITY     (4 * KiB)
 #define ZNS_PAGE_SIZE               (16 * KiB)
-#define NVME_DEFAULT_ZONE_SIZE      (64 * MiB) //72 * MiB)
+#define NVME_DEFAULT_ZONE_SIZE      (64 * MiB)
 //#define NVME_SECOND_NS_ZONE_SIZE    (64 * MiB)
 #define NVME_DEFAULT_MAX_AZ_SIZE    (128 * KiB)
 uint64_t lag = 0;
@@ -1794,11 +1794,11 @@ static void znsssd_init_params(FemuCtrl * n, struct zns_ssdparams *spp){
     /**
      * 1. SSD size  2. zone size 3. # of chnls 4. # of chnls per zone
     */
-    spp->nchnls         = 4;   //default : 8                                                   /* FIXME : = ZNS_MAX_CHANNEL channel configuration like this */
-    spp->chnls_per_zone = 4;   
+    spp->nchnls         = 8;   //default : 8                                                   /* FIXME : = ZNS_MAX_CHANNEL channel configuration like this */
+    spp->chnls_per_zone = 1;   
     spp->zones          = n->num_zones;     
-    spp->ways           = 1;    //default : 2
-    spp->ways_per_zone  = 1;    //default :==spp->ways
+    spp->ways           = 2;    //default : 2
+    spp->ways_per_zone  = 2;    //default :==spp->ways
     spp->dies_per_chip  = 1;    //default : 1
     spp->planes_per_die = 4;    //default : 4
     spp->register_model = 1;    
