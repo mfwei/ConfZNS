182e490270febedfd2f890eb3683eaf97302a2e0
Ming-Feng Wei <mfwei.personal@gmail.com>
2025-03-03 12:59:48 +0000

    Change way number in SSD structure referred by Table 2 in ConfZNS paper

--- hw/femu/zns/zns.c
+++ hw/femu/zns/zns.c
@@ -1797,7 +1797,7 @@ static void znsssd_init_params(FemuCtrl * n, struct zns_ssdparams *spp){
     spp->nchnls         = 8;   //default : 8                                                   /* FIXME : = ZNS_MAX_CHANNEL channel configuration like this */
     spp->chnls_per_zone = 8;   
     spp->zones          = n->num_zones;     
-    spp->ways           = 2;    //default : 2
+    spp->ways           = 1;    //default : 2
     spp->ways_per_zone  = 1;    //default :==spp->ways
     spp->dies_per_chip  = 1;    //default : 1
     spp->planes_per_die = 4;    //default : 4
